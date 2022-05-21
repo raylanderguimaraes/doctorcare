c43980a8e89aec64e95f11c8957f5ef0142f8277
Raylander Guimarães <92590610+raylanderguimaraes@users.noreply.github.com>
2022-05-21 11:11:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+
+window.addEventListener('scroll', onScroll);
+
+onScroll();
+
+function onScroll() {
+    showNavOnScroll();
+    showBackToTopButtonOnScroll();
+    activateMenuAtCurrentSection(home);
+    activateMenuAtCurrentSection(services);
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact);
+
+}
+
+function activateMenuAtCurrentSection(section) {
+    //linha alvo
+    const targetLine = scrollY + innerHeight / 2;
+
+    //verificar se a seção passou da linha
+    //quais dados vou precisar?
+
+    // o top da seção
+    const sectionTop = section.offsetTop;
+
+    //altura da seção
+    const sectionHeight = section.offsetHeight;
+
+    // o topo da seção chegou ou ultrapassou a linha alvo
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+    // verificar se a base está abaixo da linha alvo
+    //quais dados vou precisar?
+    const sectionEndsAt = sectionTop + sectionHeight;
+
+    // o final da seção passou da linha alvo?
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+    // informações dos dados
+    console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetline);
+
+    console.log('O fundo da seção passou a linha?', sectionEndPassedTargetline);
+
+    // limites da seção
+    const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+    console.log(sectionBoundaries);
+
+    const sectionId = section.getAttribute('id');
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+    menuElement.classList.remove('active');
+    if (sectionBoundaries) {
+        menuElement.classList.add('active');
+    }
+}
+
+
+
+function showNavOnScroll() {
+    if (scrollY > 0) {
+        navigation.classList.add('scroll');
+    } else {
+        navigation.classList.remove('scroll');
+    }
+}
+
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 550) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+}
+
+
+function openMenu() {
+    document.body.classList.add('menu-expanded');
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded');
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`#home, 
+            #home img,
+            #home .numbers,
+            #services,
+            #services header,
+            #services .card,
+            #about,
+            #about header,
+            #about .content`);
+
+
+
